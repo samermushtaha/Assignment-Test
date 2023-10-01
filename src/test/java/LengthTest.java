27b4816afefaafba5355b3c81bd5345ef98f3d88
samermushtaha <samermushtaha0@gmail.com>
2023-10-01 14:56:39 +0300

    init

---
+++ src/test/java/LengthTest.java
@@ -0,0 +1,18 @@
+import org.example.Main;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class LengthTest {
+
+    @DataProvider(name = "lengthTestDataProvider")
+    public Object[][] lengthTestDataProvider(){
+        return new Object[][] {{"samer", 5}, {"", 0}, {"sa@mer##", 8}};
+    }
+
+    @Test(dataProvider = "lengthTestDataProvider")
+    public void lengthTest(String text, int expected){
+        int actual = text.length();
+        Assert.assertEquals(expected, actual);
+    }
+}
